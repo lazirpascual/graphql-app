b70385daa07901998d93aa057b47c6733a869dcc
Lazir Pascual <lazirpascual@gmail.com>
2022-05-03 23:49:11 -0400

    add movie type and resolvers

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -1,8 +1,9 @@
-const { UserList } = require("../DummyData");
+const { UserList, MovieList } = require("../DummyData");
 const _ = require("lodash");
 
 const resolvers = {
   Query: {
+    // USER RESOLVERS
     users: () => {
       return UserList;
     },
@@ -10,6 +11,25 @@ const resolvers = {
       const id = args.id;
       const user = _.find(UserList, { id: Number(id) });
       return user;
+    },
+
+    // MOVIE RESOLVERS
+    movies: () => {
+      return MovieList;
+    },
+    movie: (parent, args) => {
+      const name = args.name;
+      const movie = _.find(MovieList, { name });
+      return movie;
+    },
+  },
+  User: {
+    favoriteMovies: () => {
+      return _.filter(
+        MovieList,
+        (movie) =>
+          movie.yearOfPublication >= 2000 && movie.yearOfPublication <= 2010
+      );
     },
   },
 };
